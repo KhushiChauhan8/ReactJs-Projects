a0a6d38b0d2442cb7028846951dae691763b993d
Khushichauhan <kc6478576@gmail.com>
2024-09-11 15:47:48 +0530

    Add reactRouter Project

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+function useCurrancyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data)
+    }, [currency])
+    console.log(data)
+    return data
+}
+
+export default  useCurrancyInfo;
---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-600 p-4'>React Router</h1>
+    </>
+  )
+}
+
+export default App
